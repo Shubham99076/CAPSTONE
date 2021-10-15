001226d1268d1b2bc8798d0ad48f49b04b32dddb
Shubham99076 <43137817+Shubham99076@users.noreply.github.com>
2021-10-15 13:35:43 +0530

    Add files via upload

---
+++ AllReports/src/main/java/com/report/AllReportsApplication.java
@@ -0,0 +1,13 @@
+package com.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllReportsApplication.class, args);
+	}
+
+}
